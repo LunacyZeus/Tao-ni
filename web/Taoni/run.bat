7ea6a69cbeb9083bbdea3a5e8c79bf8eb3e5ee9a
unknown <>
2017-02-20 18:57:29 +0800

    更新目录文件

---
+++ web/Taoni/run.bat
@@ -0,0 +1,5 @@
+@echo ---**Django应用调试**---
+@echo 小小Ho制作。
+
+@cmd
+@python manage.py runserver
---
+++ web/Taoni/tbk/urls.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+
+from django.shortcuts import render
+
+from django.conf.urls import url
+
+from . import views
+
+urlpatterns = [
+    url(r'^$', views.index, name='index'),
+]
